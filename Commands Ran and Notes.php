7cb11ec3288f0b230b14dc88ecbb09c69b2a0388
gudlukmme@gmail.com <gudlukmme@gmail.com>
2023-05-09 23:54:05 +0200

    JWT Installed

---
+++ Commands Ran and Notes.php
@@ -0,0 +1,20 @@
+<?php
+
+#composer create-project laravel/laravel="8.0" laravel_jwt_auth
+
+# composer require tymon/jwt-auth:dev-develop --prefer-source
+
+# php artisan vendor:publish
+# after this command, please press 9 and enter
+
+# php artisan jwt:secret
+
+/*
+
+PS C:\xampp\htdocs\projphp\laravel_jwt_auth> php artisan jwt:secret
+
+jwt-auth secret [OcwTv9OjccjD5OfvFmorMQenH5mJU95vE7KxFxPdOSqavcuKbZ99qXB9CgPUdo4j] set successfully.
+
+*/
+
+
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/* Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+}); */
+
+Route::group([
+    'middleware' => 'api',
+    'namespace' => 'App\Http\Controllers',
+    'prefix' => 'auth'
+], function ($router){
+    Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('profile', 'AuthController@profile');
+    Route::post('refresh', 'AuthController@refresh');
 });
